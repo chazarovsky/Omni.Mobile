5f9544ebaaf8c30ddb533ecc98b76ce8364a7f2a
Diego Plascencia <diego-d@outlook.com>
2015-06-11 01:45:35 -0500

    add Indication model (POJO), Indications Adapter, indications item  and Indications Fragment to do implementation of RecyclerView; change the fragment to be inflated in UtilActivity

---
+++ app/src/main/java/net/omnidf/omnidf/Indication.java
@@ -0,0 +1,22 @@
+package net.omnidf.omnidf;
+
+public class Indication {
+    private String type;
+    private String indication;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getIndication() {
+        return indication;
+    }
+
+    public void setIndication(String indication) {
+        this.indication = indication;
+    }
+}
---
+++ app/src/main/java/net/omnidf/omnidf/IndicationsAdapter.java
@@ -0,0 +1,52 @@
+package net.omnidf.omnidf;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class IndicationsAdapter extends RecyclerView.Adapter<IndicationsAdapter.ViewHolder>{
+
+    ArrayList<Indication> indications;
+
+    public IndicationsAdapter(ArrayList<Indication> indications) {
+        this.indications = indications;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_indications, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+
+        viewHolder.imageIndicationType.setImageResource(R.drawable.testicon);
+        viewHolder.textIndication.setText(indications.get(i).getIndication());
+    }
+
+    @Override
+    public int getItemCount() {
+        return indications.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageIndicationType;
+        TextView textIndication;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            imageIndicationType = (ImageView) itemView.findViewById(R.id.imageIndicationType);
+            textIndication = (TextView) itemView.findViewById(R.id.textIndication);
+        }
+    }
+}
